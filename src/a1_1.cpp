810edaa8339746bc9352b82fdc31a6cf1f7898ac
Luc Street <6709262+0xSiO@users.noreply.github.com>
2016-08-22 18:35:21 -0700

    Do Assignment 1.1

---
+++ src/a1_1.cpp
@@ -0,0 +1,59 @@
+/*
+ * Lucas Street, 08/22
+ * CS 10, Dave Harden
+ * Assignment 1.1 - a1_1.cpp
+ *
+ * Prints a few strings to stdout, occasionally splitting onto multiple lines.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // part a
+    cout << "Tran says this is her first computer program." << endl;
+
+    // part b
+    cout << "Tran says this is her" << endl;
+    cout << "first computer program." << endl;
+
+    // part c
+    cout << "Tran says this is her\nfirst computer program." << endl;
+
+    // part d
+    cout << "Tran says this is her ";
+    cout << "first computer program." << endl;
+
+    // part e
+    cout << "Tran said, \"This is my first computer program.\"" << endl;
+
+    // part f
+    cout << "Tran said, \"This is my" << endl;
+    cout << "first computer program.\"" << endl;
+
+    // part g
+    cout << "Tran said, \"This is my\nfirst computer program.\"" << endl;
+
+    // part h
+    cout << "Tran said, \"This is my ";
+    cout << "first computer program.\"" << endl;
+
+    return 0;
+}
+
+// Output
+/*
+Tran says this is her first computer program.
+Tran says this is her
+first computer program.
+Tran says this is her
+first computer program.
+Tran says this is her first computer program.
+Tran said, "This is my first computer program."
+Tran said, "This is my
+first computer program."
+Tran said, "This is my
+first computer program."
+Tran said, "This is my first computer program."
+*/
